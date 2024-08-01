68dbf79f69569e5c58a15fbc0445758eb0c68cbc
rcz <83134503+rcznp@users.noreply.github.com>
2024-08-02 02:44:23 +0800

    Add files via upload

---
+++ final_pytest/config.py
@@ -0,0 +1,11 @@
+import os
+parent_1 = os.path.dirname(os.getcwd())
+
+
+base_path_concat = os.path.join(os.path.dirname(parent_1),'iras_api_clean_1')
+
+log_dir = os.path.dirname(base_path_concat) + '/Full_Logs/logs_2'
+
+# work_directory = os.path.join(base_path_concat,'1_Input_Validation')
+# print(work_directory)
+
