e4fb2a23530ef9da6d3541bea1e53d30f61cf5d0
rayzheng59 <rayzheng59@gmail.com>
2024-08-03 19:03:19 +0800

    try merge to main again

--- final_pytest/pytest_main/test_p.py
+++ final_pytest/pytest_main/test_p.py
@@ -11,17 +11,3 @@ work_directory = os.path.join(base_path_concat,work_directory_folder_name)
 print('work_directory', work_directory)
 if os.path.exists(work_directory):
     print('lol')
-
-# logger1, logger2 = setup_loggers(log_dir)
-
-# xml_file_path = os.path.join(work_directory, xml_store_folder_name)
-# filename_for_csv_with_results = test_file_name + unique_identifier_for_test_results
-
-
-# filename_for_csv_with_results_full_path = os.path.join(work_directory, 'test_results', f'{filename_for_csv_with_results}.csv')
-# base_df_filepath = os.path.join(work_directory, 'df_full.csv')
-
-# if os.path.exists(filename_for_csv_with_results_full_path):
-#     os.remove(filename_for_csv_with_results_full_path)
-#     print(f"File {filename_for_csv_with_results_full_path} removed %%%.")
-# print(xml_file_path)
